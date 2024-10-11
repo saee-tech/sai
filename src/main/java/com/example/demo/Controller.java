e2434026516c3ad93046db54d0675971bf84cec1
saee-tech <lveerasai9052@gmail.com>
2024-10-11 09:48:44 +0530

    first commit

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Controller {
+	
+	@RequestMapping("/")
+	
+	public String Index()
+	
+	{
+		return "Hello World";
+	}
+	
+
+}
